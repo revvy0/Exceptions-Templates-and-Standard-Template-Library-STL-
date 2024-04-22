b259483997ca218ba7268366b5f4e2bac2003c66
revvy0 <163015200+revvy0@users.noreply.github.com>
2024-04-22 13:52:55 -0400

    Create source.cpp
    added basic functions and classes will add code later

---
+++ source.cpp
@@ -0,0 +1,27 @@
+// Paul Shelley
+// 4/22/24
+// CIS 1202.501
+// 4/22/24 adding basic classes and functions
+
+#include <iostream>
+#include <stdexcept>
+
+char character(char start, int offset);
+
+class invalidCharacterException : public exception {
+
+};
+
+class invalidRangeException : public exception {
+
+};
+
+char character(char start, int offset) {
+
+}
+
+int main () {
+
+
+  return 0;
+}
